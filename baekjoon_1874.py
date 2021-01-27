3736f9813517908a47fab903995a228f59a7eccc
sannyuni33 <sannyuni33@naver.com>
2021-01-27 20:03:01 +0900

    21.01.27
    1874 해결 완료, 시간 너무 길다

--- baekjoon_1874.py
+++ baekjoon_1874.py
@@ -2,22 +2,22 @@ n = int(input())
 init = [i for i in range(1, n+1)]
 stack, answer, res = [0], [], []
 for _ in range(n):
-    x = int(input())
-    if stack[-1] == x:
-        res.append(stack.pop())
-        answer.append('-')
-    else:
-        while stack[-1] != x:
+    res.append(int(input()))
+for x in res:
+    if stack[-1] != x:
+        while stack[-1] != x and len(init) > 0:
             stack.append(init.pop(0))
             answer.append('+')
-        res.append(stack.pop())
-        answer.append('-')
-print(res)
-for x in answer:
-    print(x)
+    if stack.pop() != x:
+        print("NO")
+        exit()
+    answer.append('-')
+for a in answer:
+    print(a)
 # 1 부터 n 까지 있는 수열을 스택가지고 입력받은 수열로 바꾸고 싶다
 # 어떤 수가 정답 수열에 더해지려면, pop 이 되야하고,
 # 그 이전엔 당연히 스택에 push 가 됐을것임 물론 걔보다 작은 수들도 마찬가지
+
 # 원하는 수열이 1 2 3 ... 100,000 이면 push() pop() 을 10만 번 하면 끝이고(20만),
 # 100,000 99,999 99,998 ... 1 이면 push() 10만 번, pop() 10만 번 이다.
 # 스택에서는 top 이 중요. top 가지고 알고리즘을 짤 수 있는지 고민
@@ -30,3 +30,8 @@ for x in answer:
 # 예제 출력 1은 맞게 나오고 예제 출력 2(NO인 경우)는 
 # 비어있는 init 에서 꺼내올려고 해서 런타임에러.
 # 어디서 어떻게 break 걸어주는게 가장 똑똑한걸까
+
+# 이미 5가 pop 되었다는 것은 init 이 텅텅 비었다는 것.
+# 그러면 3은 이미 스택에 있다는 것.
+
+# 맞긴 했는데 시간이 5636ms
