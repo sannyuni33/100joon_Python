993dc948ecd39ba26ba5220b513c226fef5a4092
sannyuni33 <sannyuni33@naver.com>
2021-02-07 16:15:08 +0900

    21.02.07
    카카오 18년도 신입공채 코테문제 5번 - 해결중
    19539 해결중
    9517 해결완료 - 폭탄 넘기는 상황을 구현할때, 모듈로 연산보다 큐 회전이 더 빨랐다?

---
+++ baekjoon_9517.py
@@ -0,0 +1,40 @@
+def turn(table):
+    table.append(table.pop(0))
+
+
+k = int(input())
+n = int(input())
+countDown = 210
+firstIsBomb = [i for i in range(1, 9)]
+for _ in range(k-1):
+    turn(firstIsBomb)
+for _ in range(n):
+    t, z = map(str, input().split())
+    countDown -= int(t)
+    if countDown <= 0:
+        ans = firstIsBomb[0]
+        break
+    if z == 'T':
+        turn(firstIsBomb)
+print(ans)
+
+# k = int(input())
+# n = int(input())
+# countDown = 210
+#
+# for _ in range(n):
+#     t, z = map(str, input().split())
+#     countDown -= int(t)
+#     if countDown <= 0:
+#         break
+#     if z == 'T':
+#         k += 1
+#         print('폭탄 받은 사람', k % 8)
+# ans = k % 8 if k % 8 != 0 else 8
+# print(ans)
+# 사람수는 8명으로 고정, 3분30초는 210초
+# 원형으로 앉아있고, 질문수는 최대 100개니까 큐로 구현하는게 좋겠고
+# 폭탄 든 사람이 정해지면 걔부터 8명을 채운다.
+# 정답을 맞추더라도 그 전에 시간이 다되면 터지니까 시간 줄이기를 먼저
+
+# 큐로 구현한 버전은 64ms, 모듈로 연산 쓴 버전은 68ms
--- kakao_2018_news_clustering.py
+++ kakao_2018_news_clustering.py
@@ -1,7 +1,9 @@
 def solution(str1, str2):
     answer = 0
+    if len(str2) < len(str1):
+        str1, str2 = str2, str1
     str1, str2 = str1.lower(), str2.lower()
-    set1, set2 = [], []
+    set1, set2, gyo = [], [], []
     for i in range(len(str1)-1):
         if str1[i].isalpha() and str1[i+1].isalpha():
             set1.append(str1[i]+str1[i+1])
@@ -10,6 +12,9 @@ def solution(str1, str2):
             set2.append(str2[i]+str2[i+1])
     print(set1)
     print(set2)
+    # for i in range(len(set1)):
+    #     for j in range(len(set2)):
+    #         if set1[i] ==
 
     return answer
 
@@ -17,4 +22,6 @@ def solution(str1, str2):
 solution("FRANCE", "french")
 
 # 중복을 허용하는 다중집합이기때문에 set() 으로 만들어버리면 곤란
-#
+# set1의 각 요소에 대해 set2의 각 요소와 비교, 일치하는애를 찾으면
+# set1과 set2에서 둘 다 pop 하고 교집합에 집어넣는다,
+# 교집합의 크기는 len(gyo), 합집합의 크기는 len(gyo)+len(set1)+len(set2)
