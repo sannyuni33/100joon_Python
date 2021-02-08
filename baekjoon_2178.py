41617d1c5fe8c782feb9f99bfc5bf4bfe25ba465
sannyuni33 <sannyuni33@naver.com>
2021-02-08 15:05:17 +0900

    21.02.08
    2178 해결완료

--- baekjoon_2178.py
+++ baekjoon_2178.py
@@ -2,7 +2,7 @@ def BFS(mat):
     queue, visited = [], [[False for _ in range(m)] for _ in range(n)]
     x, y = [0, 1, 0, -1], [1, 0, -1, 0]
     queue.append([0, 0])
-    visited[0][0], phase = True, 1
+    visited[0][0] = True
 
     while len(queue) > 0:
         tmp = queue.pop(0)
@@ -12,18 +12,16 @@ def BFS(mat):
             if 0 <= tmpX < n and 0 <= tmpY < m and not visited[tmpX][tmpY] and int(mat[tmpX][tmpY]):
                 queue.append([tmpX, tmpY])
                 visited[tmpX][tmpY] = True
-
-    print(phase)
+                mat[tmpX][tmpY] += mat[row][col]
+    print(len(mat[n-1][m-1]))
 
 
 n, m = map(int, input().split())
 matrix = [list(input()) for _ in range(n)]
-ans = 0
-
-
-print(matrix)
 BFS(matrix)
 # BFS 를 돌리고 한 칸 이동할때마다 1 더하기. n, m 도달할때까지
 # 네 방향 모두 이동 가능해야 함. 꼬불꼬불 미로일수도 있으니깐
 # BFS 로 쭈우우우욱 탐색을 하다가 다음에 (N, M)이 들어올 예정이면 마무리
 
+# '한 번 이동' 을 어떻게 구현할지 고민해봐야 함.
+# 스택에 이동가능한 칸을 넣을때, 이전단계의 값이랑 더해주기?
