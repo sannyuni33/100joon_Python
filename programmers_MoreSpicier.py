7a1952547e7e5adaf2c96f350cf0810ee4f431fe
sannyuni33 <sannyuni33@naver.com>
2021-05-06 20:40:59 +0900

    21.05.06
    프로그래머스_힙_더 맵게(heapq 모듈 활용해서 성공쓰)

--- programmers_MoreSpicier.py
+++ programmers_MoreSpicier.py
@@ -1,15 +1,14 @@
+import heapq as hq
+
 def solution(scoville, K):
     answer = 0
-    scoville.append(K)
-    scoville.sort()
-    scoville = scoville[:scoville.index(K)]
+    hq.heapify(scoville)
+    print(scoville)
 
     for i in range(len(scoville)-1):
-        scoville.sort()
         if scoville[0] >= K:
             break
-        newScov = scoville.pop(0) + 2*scoville.pop(0)
-        scoville.append(newScov)
+        hq.heappush(scoville, hq.heappop(scoville) + 2*hq.heappop(scoville))
         answer += 1
         print(scoville, answer)
 
@@ -19,10 +18,10 @@ def solution(scoville, K):
 
 
 
-# solution([1, 2, 3, 9, 10, 12], 7)
+print(solution([1, 2, 3, 9, 10, 12], 7))
 # print(solution([1, 2, 3, 7, 9, 10, 12, 3, 67, 124, 34, 21, 215, 123, 232, 57], 100000000))
-print(solution([2, 2, 2, 2, 2, 2], 42))
-
+# print(solution([2, 2, 2, 2, 2, 2], 42))
+# print(solution([4, 1, 7, 3, 8, 5], 5))
 
 # 가장 먼저 K 이상인 원소를 싹 다 뺀다.
 # 그 리스트에서 가장 작은 두 개를 뽑고, 새로운 스코빌 지수를 구해 맨 앞에 넣는다.
@@ -32,4 +31,6 @@ print(solution([2, 2, 2, 2, 2, 2], 42))
 # 힙을 구현하고 거기에 넣어서 자동으로 정렬되게끔 하는 부분이 낫겠다.
 
 # 일단 2차 시도에서는 sort는 놔두고 반복문 끝내주는 부분만 수정.
-# 테스트케이스는 모두 맞았지만 효율성 테스트는 모두 시간초과. 힙 구현 고고
+# 테스트케이스는 모두 맞았지만 효율성 테스트는 모두 시간초과. 최소힙 구현 고고
+
+# heapq 모듈 사용해서 성공!
