00b6f209fc76d9581831c0922ba3d92f0bc850be
상윤 <sannyuni33@naver.com>
2021-05-12 20:25:10 +0900

    21.05.12
    윈도우 포맷후 재설치................
    프로그래머스_힙_디스크 컨트롤러 해결완료
    jobs 맨 끝에 요청시점이 inf인 작업을 추가해서,
    모든작업이 스케줄러에 들어가면
    스케줄러에 남아있는 작업들을 수행하게끔 수정

--- programmers_DiskController.py
+++ programmers_DiskController.py
@@ -1,40 +1,25 @@
-import heapq as hq
-
-
 def solution(jobs):
+    jobs.append([float('inf'), 0])
     ans1 = ans2 = currTime = 0
     scheduler = []
-    # hq.heapify(scheduler)  # 작업 대기중인 애들
-    # jobs.sort()  # 작업 요청시점으로 정렬 안된 테스트 케이스가 있다면 이 주석을 푸삼!
-    while jobs or scheduler:
+    jobs.sort()  # 작업 요청시점으로 정렬 안된 테스트 케이스가 있다면 이 주석을 푸삼!
+    while len(jobs) > 1 or scheduler:
 
         if jobs[0][0] <= currTime:  # 현재 시간보다 요청시간이 빠른작업은 모두 대기
-            print('before scheduling:', jobs)
             tmp = jobs.pop(0)
             scheduler.append(tmp[::-1])
-            # hq.heappush(scheduler, tmp[::-1])  # 소요시간을 기준으로 힙 구성
-            print('after scheduling:', jobs)
+
         else:
             if scheduler:  # 이미 요청시간 저기한 애들만 들어와있음.
-                # completed = hq.heappop(scheduler)
                 scheduler.sort()
-                print('scheduler:', scheduler)
                 completed = scheduler.pop(0)
-                print(completed, 'has complete')
                 ans1 += currTime + completed[0] - completed[1]
                 ans2 += 1  # 완료된 작업 수
                 currTime += completed[0]
-                print('time:', currTime)
             else:  # 대기중인 작업 없음
                 currTime += 1
-                print('time:', currTime)
             continue
-        if not len(jobs):
-            # break 가 아니라 스케줄러에 남아있는 애들 처리해야 함
-            break
-    print(ans1 // ans2)
     return ans1 // ans2
-
 
 solution([[0, 3], [1, 9], [2, 6]])
 # solution([[0, 3], [20, 9], [10, 6], [2, 5], [5, 8]])
